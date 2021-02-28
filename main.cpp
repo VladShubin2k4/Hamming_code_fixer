852766ca6bdea6a8b7489697c34c6536303589d5
Vlad <VladShubin2k4@gmail.com>
2021-02-28 21:41:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,52 +1,48 @@
 #include <cstring>
 #include <iostream>
-#include <fstream>
 
-char input[1024];
-int find_mistake(int& len){
-    int i, mistake = 0;
-    for (i = 0; i < len; ++i)
+short find_mistake(short& len,char* input){
+    short i, mistake = 0;
+    for (i=0; i<len; ++i)
         if (input[i] == '1')
-            mistake ^= i + 1;
+            mistake ^= i+1;
     return mistake;
 }
 
-bool check_the_digits(int& len){
+bool check_the_digits(short& len,char* input){
     for (int i = 0; i < len; ++i)  if (input[i] != '0' && input[i] != '1') return false;
     return true;
 }
 
-void out_res(int& len, int& info_pos, int& b, char* info){
-    std::cout<<"Fixed code: "<<input<<std::endl;
+void out_res(short& len, short& info_pos, short& b, char* input){
+    char info[128]={};
+    std::cout<<"Fixed code: "<<input<<"\n";
     for (int i = 0; i < len; ++i)
         if (i+1 == b) b<<=1;
         else info[info_pos++] = input[i];
     info[info_pos] = '\0';
+    std::cout<<"Informational code: "<<info<<std::endl;
 }
 
-void fix(int& mistake, int& len){
+void fix(short& mistake, short& len, char* input){
     if (mistake){
         input[mistake - 1] ^= '0' ^ '1';
-        mistake = find_mistake(len);
-        if (mistake) std::cout<<"Invalid code"<<std::endl;
+        mistake = find_mistake(len,input);
     }
 }
 
 int main(){
     std::ios::sync_with_stdio(false);
-    char info[1024];
-    int len, mistake = 0,info_pos = 0, b = 1;
-    std::fstream f; f.open("res.txt",std::ios::app | std::ios::out);
+    short len, mistake=0,info_pos=0, b=1;
+    char input[128]={};
     std::cout<<"Enter code: ";
-    std::cin >> input;
-    len = static_cast<int>(strlen(input));
-    mistake = find_mistake(len);
+    std::cin>>input;
+    len=static_cast<short>(strlen(input));
+    mistake = find_mistake(len,input);
 
-    if(check_the_digits(len)){
-        fix(mistake,len);
-        out_res(len,info_pos,b,info);
-        f<<info;
+    if(check_the_digits(len,input)){
+        fix(mistake,len,input);
+        out_res(len,info_pos,b,input);
     }else std::cout<<"Invalid code"<<std::endl;
-    f.close();
     return 0;
 }
